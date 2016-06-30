f0eecb2d2db978537a6b459c2323a5dbb1234ee5
NatoliChris <natolichris@gmail.com>
2016-06-30 14:15:24 +1000

    updating marksum -> now easy input of decimal numbers. 2 digit decimal has q infront (very close for laptop)

--- marksum.py
+++ marksum.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
+import sys
 
-import math
+marks_list = []
 
-def main():
-    count = 0
-    while(1):
-        try:
-            num = input()
-            x = int(num)
-        except:
-            break
-        if(x == -1):
-            break
-        count = count + x
-    print(count)
+def mark_sum():
+    while(True):
+        print("Please input your marks")
+        marks = 0.0
+        for line in sys.stdin:
+            line = line.replace("`", "1")
+            if(len(line) == 1):
+                print('Sum: {0:.2f}'.format(marks))
+                break
+            try:
+                if(len(line) == 4 and '.' not in line and 'q' not in line):
+                    marks += float(line)/10
+                elif('q' in line):
+                    marks += float(line.replace('q',''))/10
+                else:
+                    marks += float(line)
+            except:
+                print("")
 
-#if __name__ == "__main__":
-main()
+def average_marks():
+    print("Please input your marks")
+    for line in sys.stdin:
+        if(len(line) == 1):
+            print('AVG: {0:.2f}'.format((sum(marks_list)/ float(len(marks_list)))))
+            sys.exit(0)
+        marks_list.append(float(line))
+
+if(__name__ == '__main__'):
+    if(len(sys.argv) == 1):
+        mark_sum()
+    elif(sys.argv[1] == '-a'):
+        average_marks()
+
