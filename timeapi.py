a868afba78b5face822a0c67602fe025b983210d
NatoliChris <natolichris@gmail.com>
2017-01-08 23:59:41 +0000

    updating api with some useful info about timezone and y/m/d

--- timeapi.py
+++ timeapi.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import datetime
+import dateutil
+from dateutil.tz import *
 
 app = Flask(__name__)
 
@@ -8,9 +10,18 @@ def index():
     return "Hello, World"
 
 @app.route('/api/time', methods=['GET'])
-def get_tasks():
-    return jsonify({"time" : str(datetime.datetime.now().time()) })
+def get_time():
+
+    t = datetime.datetime.now(tzlocal()).astimezone(tzoffset('AEST', 39600))
+    time_obj = {
+        'time' : str(t.time()),
+        'timezone' : str(t.tzname()),
+        'year' : str(t.year),
+        'month' : str(t.month),
+        'day' : str(t.day),
+            }
+    return jsonify(time_obj)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)time()
 
