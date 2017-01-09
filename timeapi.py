6436df8770d0db49a0151e9e0702164f68710d37
NatoliChris <natolichris@gmail.com>
2017-01-09 00:08:15 +0000

    Updating with custom timezone

--- timeapi.py
+++ timeapi.py
@@ -19,9 +19,23 @@ def get_time():
         'year' : str(t.year),
         'month' : str(t.month),
         'day' : str(t.day),
-            }
+    }
     return jsonify(time_obj)
 
+@app.route('/api/mytime/<int:timezone>', methods=['GET'])
+def get_mytime(timezone):
+    adj = timezone*60*60
+    t = datetime.datetime.now(tzlocal()).astimezone(tzoffset('CUSTOM', adj))
+    time_obj = {
+        'time' : str(t.time()),
+        'timezone' : str(t.tzname()),
+        'year' : str(t.year),
+        'month' : str(t.month),
+        'day' : str(t.day),
+    }
+    return jsonify(time_obj)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)time()
+    app.run(debug=True)
 
